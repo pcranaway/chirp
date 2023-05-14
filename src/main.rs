382d5b402191f23f1968ec5a31f531a151bf85d0
alex <pcranaway@tuta.io>
2023-05-14 14:54:06 +0300

    init

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use std::{fs::File, io::Read};
+
+use vm::VM;
+
+pub mod vm;
+
+fn main() -> anyhow::Result<()> {
+    // create vm
+    let mut vm = VM::default();
+
+    // read program from cli arguments
+    let path = std::env::args().nth(1).expect("no file path given");
+
+    let mut file = File::open(path)?;
+    let mut program = Vec::new();
+    file.read_to_end(&mut program)?;
+
+    // load program
+    vm.load_program(program);
+
+    loop {
+        vm.tick();
+    }
+}
---
+++ src/vm.rs
@@ -0,0 +1,32 @@
+/// A VM capable of executing CHIP-8 instructions. 
+pub struct VM {
+    /// This is the memory of the VM. It's RAM and 4KB big.
+    pub memory: [u8; 4096],
+
+    /// This is the location of the current instruction in the memory.
+    pub current_instruction_idx: usize,
+}
+
+impl VM {
+    /// Loads a program from a Vec<u8> into the memory of the VM.
+    pub fn load_program(&mut self, program: Vec<u8>) {
+        self.memory[..program.len()].copy_from_slice(&program);
+    }
+
+    /// Executes the current instruction of the VM.
+    pub fn tick(&mut self) {
+        let instruction = self.memory[self.current_instruction_idx];
+
+        println!("Instruction: {:#04X?}", instruction);
+    }
+}
+
+impl Default for VM {
+    /// Creates a default instance of the CHIP-8 VM.
+    fn default() -> Self {
+        return Self {
+            memory: [0; 4096],
+            current_instruction_idx: 0,
+        };
+    }
+}
